61bf666c4db651ac5b9264bce018bb683b4e72cd
Your Name <you@example.com>
2026-03-11 02:17:00 +0530

    Fix remaining invalid href in Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       <a href='/' className='footer__logo'>VINIT</a>
 
       <ul className='permalinks'>
-        <li><a href='#'>Home</a></li>
+        <li><a href='/'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#services'>Education</a></li>
